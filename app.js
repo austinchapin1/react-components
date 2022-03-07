0404bd24c0da6509a94b3280fc693fefe9f2c345
Austin Chapin <austinchapin04@gmail.com>
2022-03-07 13:17:37 -0800

    App component, List component, Item component

--- app.js
+++ app.js
@@ -1,1 +1,74 @@
 // TODO
+var App = () => (
+  <div>
+    <h2>Grocery List</h2>
+    <GroceryList items={['Cucumbers', 'Pickles', 'Apple']}/>
+  </div>
+);
+
+var GroceryList = (props) => (
+  <ul>
+    {props.items.map( item => (
+      <GroceryListItem vegetable={item} candy={'trix'}/>
+    ))}
+  </ul>
+);
+
+
+var GroceryListItem = (props) => (
+  <li>{props.vegetable}</li>
+)
+
+
+ReactDOM.render(<App />, document.getElementById('app'));
+
+
+
+
+
+
+
+
+// ----------------------// ES6 Below
+
+
+
+// class App {
+
+//   constructor () {
+//     <div>
+//     <h2>Grocery List</h2>
+//     <GroceryList items={['Cucumbers', 'Pickles']}/>
+//   </div>
+//   }
+// };
+
+
+// class GroceryList extends React.Component {
+
+//   constructor (props) {
+//   <ul>
+//     {props.items.map(item =>
+//       <GroceryListItem item={item} />
+//     )}
+//   </ul>
+//   };
+// };
+
+// class GroceryListItem extends React.Component {
+
+//   constructor (props) {
+//     super(props);
+//   }
+//   render () {
+//     <li>{this.props.todo}</li>
+//   }
+// }
+
+// <GroceryListItem vegetable={cucumber} />
+
+// // var cucumber = new GroceryListItem ();
+// // var pickles = new GroceryListItem ();
+// // var List = new GroceryList ();
+
+// ReactDOM.render(<App />, document.getElementById('app'));
